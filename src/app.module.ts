9ee947a75658a505b5a0236df430365b23b1dbac
kohei <kouhei.ikeda@veriserve.co.jp>
2023-05-16 19:29:20 +0900

    Prisma module、serviceを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { ItemsModule } from "./items/items.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
   controllers: [],
-  imports: [ItemsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ItemsModule, PrismaModule],
   providers: [],
 })
 export class AppModule {}
